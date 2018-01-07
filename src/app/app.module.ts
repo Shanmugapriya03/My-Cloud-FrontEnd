9a1f70631fd7c1e16d951e4ba4d46204af996cae
Shanmugapriya03 <priya97a@gmail.com>
2018-01-07 16:50:56 +0530

    login communicating with serve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,6 +21,8 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     RouterModule.forRoot([
         {path : '' , component : WelcomeComponent},
         {path : 'login' , component : LoginComponent},
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http,Response } from '@angular/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http:Http) {
+  }
+  username='';
+  password='';
+  login(){
+    this.http.get('http://localhost:8000/login?userName='+this.username+'&pass='+this.password)
+      .subscribe(
+        (res:Response) =>{
+          console.log(res.json())
+        }
+      );
+  }
   ngOnInit() {
   }
 
