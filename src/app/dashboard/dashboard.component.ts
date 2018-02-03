05dc1ae03fbbf197871bd24b7d6707a6b2cea470
Shanmugapriya03 <priya97a@gmail.com>
2018-02-03 22:59:43 +0530

    changed ui

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,26 +11,7 @@ export class DashboardComponent implements OnInit {
   containers=[];
   data=[];
   constructor(private http:Http) {
-    this.containers=[
-      {
-        name:'ubuntu',
-        ip:'124.51.32.1',
-        created:'10 min',
-        status:'Running'
-      },
-      {
-        name:'hello-world',
-        ip:'142.15.32.4',
-        created:'40 min',
-        status:'Running'
-      },
-      {
-        name:'website',
-        ip:'121.5.72.6',
-        created:'10 min',
-        status:'stopped'
-      }
-    ];
+    
   }
   ngOnInit() {
     this.http.get('http://localhost:8000/dashboard')
